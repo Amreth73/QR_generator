9efd1777fa727f6fd0b24ac2a7ab8f0a5cce5bd5
Amreth73 <amreth73@gmail.com>
2024-02-06 12:31:31 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import QR_Gen from './Components/QR_code';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <QR_Gen />
   </React.StrictMode>
 );
 
